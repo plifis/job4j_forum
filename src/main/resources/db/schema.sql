8f2d9c50f03362cb94545ae6e002053e855a8093
plifis <nikich85@inbox.ru>
2021-10-05 21:26:14 +0300

    0. Spring Liquibase [#302330]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,14 +1,15 @@
-create table posts (
-                       id serial primary key,
-                       name varchar(2000),
-                       description text,
-                        comment_id int references comments(id),
-                       created timestamp without time zone not null default now()
-);
+
 create table comments (
                        id serial primary key,
                        comment varchar(5000),
                        author varchar(100),
+                       created timestamp without time zone not null default now()
+);
+create table posts (
+                       id serial primary key,
+                       name varchar(2000),
+                       description text,
+                       comment_id int references comments(id),
                        created timestamp without time zone not null default now()
 );
 
@@ -26,5 +27,3 @@ CREATE TABLE users (
                        authority_id int not null references authorities(id)
 );
 
-insert into authorities (authority) values ('ROLE_USER');
-insert into authorities (authority) values ('ROLE_ADMIN');
